c08c1ba39833feab16739f0ea94e4e38c99ea0d0
ViktorNoskire <vikkerinho@gmail.com>
2025-04-10 16:06:25 +0200

    Create a fake db with highscores

--- backend/index.js
+++ backend/index.js
@@ -20,12 +20,21 @@ app.get('/', async (req, res) => {
   res.send(html);
 });
 
-app.get('/about');
+app.get('/about', (req, res) => {});
 
-app.get('/highscore');
+app.get('/highscore', (req, res) => {});
 
 let correctWord = '';
+let startTime = null;
+let endTime = null;
+let duration = null;
 
+const HIGHCORES = [];
+
+/* //////////
+
+  Returns a randomized word
+*/ //////////
 app.post('/api/word', async (req, res) => {
   const amount = req.body.charMount;
   const unique = req.body.unique;
@@ -39,21 +48,43 @@ app.post('/api/word', async (req, res) => {
   } else {
     correctWord = chosenWord;
     console.log(correctWord);
+    startTime = new Date();
 
     res.status(200).json({ msg: "Don't try to cheat!" });
   }
 });
 
+/* //////////
+
+  Check the words equality
+*/ //////////
 app.post('/api/check-word', (req, res) => {
   let chosen = correctWord;
   let guessed = req.body.guessed;
-  const result = wordle(chosen, guessed);
+  const payload = wordle(chosen, guessed);
 
-  res.status(201).json({ data: result });
+  if (payload.result === true) {
+    endTime = new Date();
+    duration = Math.round((endTime - startTime) / 1000);
+  }
+
+  res.status(201).json({ data: payload });
 });
 
-app.use('/feedback', express.static('./feedback'));
-app.use('/words', express.static('./words'));
+/* //////////
+
+  Returns a randomized word
+*/ //////////
+app.post('/api/user-info', (req, res) => {
+  const user = req.body.user;
+  HIGHCORES.push({
+    username: user,
+    score: Math.round(duration * 4.5),
+    time: duration,
+  });
+  console.log(HIGHCORES);
+});
+
 app.use('/assets', express.static('../frontend/dist/assets'));
 
 app.listen(5080);
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import GuessList from './components/GuessList.jsx';
 function App() {
   const [window, setWindow] = useState('start');
   const [answers, setAnswers] = useState([]);
+  const [user, setUser] = useState('');
 
   if (window == 'start') {
     return (
@@ -39,13 +40,28 @@ function App() {
   } else {
     return (
       <main>
-        <h2>Du vann!</h2>
-        <input type='text' placeholder='Skriv ditt namn' />
+        <h2>You Won!</h2>
+        <input
+          type='text'
+          placeholder='Skriv ditt namn'
+          onChange={e => {
+            setUser(e.target.value);
+          }}
+        />
         <button
-          onClick={() => {
+          onClick={async () => {
+            const response = fetch('/api/user-info', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({
+                user,
+              }),
+            });
             setWindow('start');
           }}>
-          Gå till huvudmeny
+          Go to main menu
         </button>
       </main>
     );
