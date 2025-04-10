c8bce63fcda9bbe638d359155e20c198280922e8
ViktorNoskire <vikkerinho@gmail.com>
2025-04-10 15:42:23 +0200

    Change window based on which game state the user is on

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,31 +4,52 @@ import GuessForm from './components/GuessForm.jsx';
 import GuessList from './components/GuessList.jsx';
 
 function App() {
+  const [window, setWindow] = useState('start');
   const [answers, setAnswers] = useState([]);
 
-  return (
-    <main>
-      <SettingsForm
-        onSubmit={() => {
-          setAnswers([]);
-        }}
-      />
-      <GuessForm
-        onGuess={async response => {
-          const { data } = await response.json();
-          if (data.result == true) {
-            setAnswers([...answers, data.obj]);
-            console.log('yay');
-          } else if (data.result == false) {
-            console.log('nay');
-          } else {
-            setAnswers([...answers, data.obj]);
-          }
-        }}
-      />
-      <GuessList answers={answers} />
-    </main>
-  );
+  if (window == 'start') {
+    return (
+      <main>
+        <SettingsForm
+          onSubmit={() => {
+            setAnswers([]);
+            setWindow('playing');
+          }}
+        />
+      </main>
+    );
+  } else if (window == 'playing') {
+    return (
+      <main>
+        <GuessForm
+          onGuess={async response => {
+            const { data } = await response.json();
+            if (data.result == true) {
+              setAnswers([...answers, data.obj]);
+              setWindow('end');
+            } else if (data.result == false) {
+            } else {
+              setAnswers([...answers, data.obj]);
+            }
+          }}
+        />
+        <GuessList answers={answers} />
+      </main>
+    );
+  } else {
+    return (
+      <main>
+        <h2>Du vann!</h2>
+        <input type='text' placeholder='Skriv ditt namn' />
+        <button
+          onClick={() => {
+            setWindow('start');
+          }}>
+          Gå till huvudmeny
+        </button>
+      </main>
+    );
+  }
 }
 
 export default App;
